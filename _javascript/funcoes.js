e89cbe0122006a4a06018b5ab6eee6cd56026d0e
Renan Rodolfo da Silva <renaner123@gmail.com>
2020-09-17 21:59:24 -0300

    Finalizando formulário

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,14 @@
 function mudaFoto (foto){
     document.getElementById("icone").src = foto;
 }
+
+function calc_total(){
+    var quantidade = document.getElementById("cQuantidade");
+    var total = document.getElementById("cPreco");
+    var valoroculos = 1300;
+
+    var qtd = parseInt(quantidade.value);
+
+    total.value = qtd*valoroculos;
+
+}
